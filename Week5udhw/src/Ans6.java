d2fb44924fbf8d806cb939b4d1d892e0f5cc4046
Khichiyaking <dpatel15696@gmail.com>
2024-04-07 21:44:06 +0100

    ans7

---
+++ Week5udhw/src/Ans6.java
@@ -0,0 +1,14 @@
+public class Ans6 {
+    //Write a Java program to print the sum of two numbers.
+    //Test Data: 74 + 36
+    //Expected Output: 110
+    public static void main(String[] args) {
+
+        int x=74;
+        int y=36;
+        int sum = x+y;
+
+        System.out.println("Expected output" +sum);
+    }
+
+}
