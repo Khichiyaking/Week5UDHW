44bfce7cddcff0289c9f5d57b3d7840a7016c4dd
Khichiyaking <dpatel15696@gmail.com>
2024-04-08 20:47:14 +0100

    push 2 massage Ans10-11

---
+++ Week5udhw/src/Ans10.java
@@ -0,0 +1,20 @@
+public class Ans10 {
+    //10. Write a Java program that takes two numbers as input and display the
+    //product of two numbers.
+    //Test Data:
+    //Input first number: 25
+    //Input second number: 5
+   // Expected Output : 25 x 5 = 125
+
+    public static void main(String[] args) {
+
+        int X=25;
+        int Y=5;
+        int total=X*Y;
+
+               System.out.println("Expected Output 25x5=" +total);
+    }
+
+
+
+}
