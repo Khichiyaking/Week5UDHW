85b68b8d7e36009b56480f4b9e0d59de52938d1d
Khichiyaking <dpatel15696@gmail.com>
2024-04-08 20:54:11 +0100

    push 2 massage Ans14

---
+++ Week5udhw/src/Ans14.java
@@ -0,0 +1,8 @@
+public class Ans14 {
+    public static void main(String[] args) {
+
+        System.out.println("25 != 39\n" +
+                "25 < 39\n" +
+                "25 <= 39");
+    }
+}
