16541df3d70528889018ee68e306f7a23d1dabd7
Khichiyaking <dpatel15696@gmail.com>
2024-04-08 21:06:58 +0100

    push 2 massage Ans16

---
+++ Week5udhw/src/Ans16method.java
@@ -0,0 +1,14 @@
+//16. Write program to display
+// Test Data: ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+// Expected Output 2.138888888888889
+public class Ans16method {
+    public static void main(String[] args) {
+
+        float a = 25.5f;
+        float b = 3.5f;
+        float c = 40.5f;
+        float d = 4.5f;
+
+        System.out.println((a*b-b*b)/(c-d));
+    }
+}
