bb27f7e91f0d13375d3fc2836cd0d307d5853ffa
Khichiyaking <dpatel15696@gmail.com>
2024-04-10 09:14:23 +0100

    push Week6

---
+++ Week6homework/src/NewInstanceVariables/Week6Qus15.java
@@ -0,0 +1,19 @@
+package NewInstanceVariables;
+
+import java.sql.SQLOutput;
+
+// 1.5 Call the above instance method into the Main method and run the programme.
+public class Week6Qus15 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Divyam");
+
+        Week6Qus15 a = new Week6Qus15();
+        a.name();
+    }
+    public void name() {
+        System.out.println("Patel");
+
+    }
+}
