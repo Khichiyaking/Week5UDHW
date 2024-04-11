7b838524d083e902c2670c3485f059e80203efd5
Khichiyaking <dpatel15696@gmail.com>
2024-04-11 08:53:41 +0100

    push Week6

---
+++ Week6homework/src/StaticVariable/Week6Qus24.java
@@ -0,0 +1,11 @@
+package StaticVariable;
+//2.4 Declare the Main method.
+public class Week6Qus24 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Dhaval");
+        System.out.println("Patel");
+    }
+
+}
---
+++ Week6homework/src/StaticVariable/Week6Qus25.java
@@ -0,0 +1,12 @@
+package StaticVariable;
+
+public class Week6Qus25 {
+    public static void main(String[] args) {
+    user(); // access userDefined static method to main method
+}
+
+    // Instance Methods
+    public static void user(){
+        System.out.println("Dhaval");
+    }
+}
