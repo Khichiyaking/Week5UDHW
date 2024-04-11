24be7d120218673c9de8b0f6a2ffa0f6de5fa396
Khichiyaking <dpatel15696@gmail.com>
2024-04-11 10:01:41 +0100

    push Qus11

---
+++ Week6homework/src/StaticVariable/Qus15.java
@@ -0,0 +1,10 @@
+package StaticVariable;
+//15. Write a Java program to convert a given string into lowercase.
+//Sample Input: THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+public class Qus15 {
+    public static void main(String[] args) {
+        String txt = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
+        System.out.println(txt.toLowerCase());}
+
+
+}
